adc5023121c8cfd0f9c699b0098863bc64b60c5d
sk392 <sdhfdpdp7s@gmail.com>
2021-11-20 15:46:30 +0900

    #2- 검색 기본 화면구성 추가

--- app/src/main/java/com/example/gittestapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/gittestapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.gittestapplication
+package com.example.gittestapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gittestapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
