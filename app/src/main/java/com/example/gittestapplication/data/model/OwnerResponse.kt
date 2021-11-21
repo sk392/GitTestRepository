74eb46c74c24fd9113824d7890a2172079c79788
sk392 <sdhfdpdp7s@gmail.com>
2021-11-21 23:33:54 +0900

    #10- Response모델에 SerializedName추가

--- app/src/main/java/com/example/gittestapplication/data/model/OwnerResponse.kt
+++ app/src/main/java/com/example/gittestapplication/data/model/OwnerResponse.kt
@@ -1,26 +1,27 @@
 package com.example.gittestapplication.data.model
 
 import com.example.gittestapplication.domain.model.Owner
+import com.google.gson.annotations.SerializedName
 
 data class OwnerResponse(
-    val id: Int?,
-    val login: String?,
-    val url: String?,
-    val avatarUrl: String?,
-    val eventsUrl: String?,
-    val followersUrl: String?,
-    val followingUrl: String?,
-    val gistsUrl: String?,
-    val gravatarId: String?,
-    val htmlUrl: String?,
-    val nodeId: String?,
-    val organizationsUrl: String?,
-    val receivedEventsUrl: String?,
-    val reposUrl: String?,
-    val siteAdmin: Boolean?,
-    val starredUrl: String?,
-    val subscriptionsUrl: String?,
-    val type: String?
+    @SerializedName("id") val id: Int?,
+    @SerializedName("login") val login: String?,
+    @SerializedName("url") val url: String?,
+    @SerializedName("avatar_url") val avatarUrl: String?,
+    @SerializedName("events_url") val eventsUrl: String?,
+    @SerializedName("followers_url") val followersUrl: String?,
+    @SerializedName("following_url") val followingUrl: String?,
+    @SerializedName("gists_url") val gistsUrl: String?,
+    @SerializedName("gravatar_id") val gravatarId: String?,
+    @SerializedName("html_url") val htmlUrl: String?,
+    @SerializedName("node_id") val nodeId: String?,
+    @SerializedName("organizations_url") val organizationsUrl: String?,
+    @SerializedName("receivedEvents_url") val receivedEventsUrl: String?,
+    @SerializedName("repos_url") val reposUrl: String?,
+    @SerializedName("site_admin") val siteAdmin: Boolean?,
+    @SerializedName("starred_url") val starredUrl: String?,
+    @SerializedName("subscriptions_url") val subscriptionsUrl: String?,
+    @SerializedName("type") val type: String?
 )
 
 fun OwnerResponse.toEntity(): Owner? {
