08c285e6d6f7efc430780c92c51e183bff4a2954
sk392 <sdhfdpdp7s@gmail.com>
2021-11-21 22:30:16 +0900

    #3- 도메인 모델 non-null 값 수정

--- app/src/main/java/com/example/gittestapplication/ui/model/RepositoryUIModel.kt
+++ app/src/main/java/com/example/gittestapplication/ui/model/RepositoryUIModel.kt
@@ -6,7 +6,6 @@ data class RepositoryUIModel(
     val repositoryId: Int,
     val repositoryUrl: String,
     val repositoryName: String,
-    val repositoryFullName: String,
     val userId: Int,
     val userName: String,
     val userUrl: String
@@ -22,7 +21,6 @@ fun Repository.toUIModel() = RepositoryUIModel(
     repositoryId = id,
     repositoryUrl = url,
     repositoryName = name,
-    repositoryFullName = fullName,
     userId = owner.id,
     userName = owner.login,
     userUrl = owner.url
