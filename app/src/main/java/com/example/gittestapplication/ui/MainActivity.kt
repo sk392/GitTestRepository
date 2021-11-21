1316fddc073375b9f50bf5892c4686af887f75d8
sk392 <sdhfdpdp7s@gmail.com>
2021-11-21 19:20:10 +0900

    #2- ViewModel에서 값을 가져와 노출 할 수 있도록 View, observer 추가

--- app/src/main/java/com/example/gittestapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/gittestapplication/ui/MainActivity.kt
@@ -2,11 +2,52 @@ package com.example.gittestapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gittestapplication.R
+import com.example.gittestapplication.databinding.ActivityMainBinding
+import com.example.gittestapplication.ui.adapter.MainAdapter
+import com.example.gittestapplication.ui.viewmodel.MainViewModel
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by inject()
+    private val mainAdapter: MainAdapter = MainAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        initView()
+        initObservers()
+    }
+
+    private fun initView() {
+        binding.rvResult.apply {
+            itemAnimator = null
+            layoutManager = LinearLayoutManager(context)
+            adapter = mainAdapter
+        }
+
+        binding.btSearch.apply {
+            setOnClickListener {
+                search(binding.etSearch.text.toString())
+            }
+        }
+    }
+
+    private fun search(query: String){
+        viewModel.fetch(query)
+    }
+
+    private fun initObservers() {
+        viewModel.fetchRepositories.observe(this, {
+            mainAdapter.submitList(it)
+        })
     }
 }
