02651cae1e83f87b9cb3769be929133326000b6a
sk392 <sdhfdpdp7s@gmail.com>
2021-11-21 16:51:19 +0900

    #2- 뷰모델에서 임시 리스트 뱉어줄 수 있도록 함수 추가

---
+++ app/src/main/java/com/example/gittestapplication/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.gittestapplication.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gittestapplication.domain.model.Repository
+import com.example.gittestapplication.ui.model.RepositoryUIModel
+import com.example.gittestapplication.ui.model.toUIModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _fetchRepositories = MutableLiveData<List<RepositoryUIModel>>()
+    val fetchRepositories: LiveData<List<RepositoryUIModel>> = _fetchRepositories
+
+    fun fetch(query: String) {
+        viewModelScope.launch {
+            _fetchRepositories.postValue(fetchRepositoriesUseCase((query)))
+        }
+    }
+
+    private suspend fun fetchRepositoriesUseCase(query: String): List<RepositoryUIModel> {
+        delay(1000)
+        return mutableListOf<RepositoryUIModel>().apply {
+            for (i in 0..100) {
+                add(Repository(i).toUIModel())
+            }
+        }
+    }
+}
